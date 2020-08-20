b6a08bbfbaea25f07f0ec38f60e5de9e5464b07c
Ben Wunderlich <ben.wunderlich@outloo.com>
2020-08-20 00:08:54 -0700

    adding map marker selection
    can also now toggle between mouse modes
    replacing marker that didnt have transparent background

--- drag.js
+++ drag.js
@@ -12,8 +12,12 @@ function dragElement(elmnt) {
   }
 
   function dragMouseDown(e) {
+    if ($(elmnt).hasClass('dontDrag')){
+      console.log("is viewing")
+      return;
+    }
     e = e || window.event;
-    e.preventDefault();
+    //e.preventDefault();
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
@@ -24,7 +28,7 @@ function dragElement(elmnt) {
 
   function elementDrag(e) {
     e = e || window.event;
-    e.preventDefault();
+    //e.preventDefault();
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
@@ -41,3 +45,5 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+dragElement($('#mapscreen')[0]);
--- start.js
+++ start.js
@@ -1,14 +1,13 @@
 //dragElement(document.getElementById("mapscreen"));
-dragElement($('#mapscreen')[0]);
 
 const electron = require('electron');
 const {ipcRenderer} = electron;
 const dialog = electron.remote.dialog;
 var sizeOf = require('image-size');
 
-//const fs = require('fs');
+const fs = require('fs');
 
-var projJson;
+//var projJson;
 var currentMarkerIcon;
 
 //can be view, add or edit
@@ -24,45 +23,92 @@ function changeBackgroundImage(url){
     });
 }
 
+function makeMarkers(){
+    fs.readdirSync('map_markers\\').forEach(file => {
+        file = 'map_markers/'.concat(file);
+        console.log(file);
+        var newImg = document.createElement("IMG");
+        newImg.setAttribute('src', file)
+        $(newImg).on('click', function(){
+            currentMarkerIcon = file;
+            $('#icons').children().removeClass("currentMarker");
+            newImg.classList.add("currentMarker");
+        });
+        $('#icons').append(newImg);
+    });
+}
 
-function loadIcons(projectName){
+
+function loadIcons(projectInfo){
     //clear previous markers
     $('#mapmarkers').empty();
 
     //add icons from project
+    const markers = projectInfo[1].markers;
     //XXX
 }
 
-function loadProject(projectName){
-    console.log("now opening ".concat(projectName));
+function loadProject(projectInfo){
+    console.log("now opening ".concat(projectInfo[0]));
 
-    const imgDir = 'projects/'+projectName+'/image.jpg'
+    const imgDir = 'projects/'+projectInfo[0]+'/image.jpg'
 
     //load main image
     changeBackgroundImage(imgDir);
 
     //load icons on image
-    loadIcons(projectName);
+    loadIcons(projectInfo);
     $('#rightbar').css('visibility', 'visible');
     $('#welcomeMessage').css('visibility', 'hidden');
 }
 
+function addMarker(currX, currY){
+    console.log("X is"+currX);
+    console.log("Y is"+currY);
+    //XXX
+}
+
 //projInfo has [name, json]
 ipcRenderer.on('project:open', function(e,projInfo){
-    loadProject(projInfo[0], projInfo[1])
+    loadProject(projInfo)
     console.log(projInfo[1])
     console.log("is it json? "+projInfo[1].title);
-    projJson = projInfo[1];
+    //projJson = projInfo[1];
 });
 
-$('#viewMouse').click(function(){
-    mouseMode="view";
+
+$('#viewMouse').on('click', function(){
+    mouseMode='view';
+    $('#mapscreen').removeClass('dontDrag')
 });
 
-$('#addMouse').click(function(){
-    mouseMode="add";
+$('#addMouse').on('click', function(){
+    mouseMode='add';
+    if(!$('#mapscreen').hasClass('dontDrag')){
+        $('#mapscreen').addClass('dontDrag');
+    }
 });
 
-$('#editMouse').click(function(){
-    mouseMode="edit";
+$('#editMouse').on('click', function(){
+    mouseMode='edit';
+    if(!$('#mapscreen').hasClass('dontDrag')){
+        $('#mapscreen').addClass('dontDrag');
+    }
 });
+
+
+$('#mapscreen').click(function(e){
+    switch (mouseMode) {
+        case 'add': 
+            addMarker(e.clientX, e.clientY);
+            break;
+        case 'view':
+            break;
+        case 'edit':
+            break;
+        default:
+            throw 'mouse mode in something other than view, edit or add';
+    }
+});
+
+makeMarkers();
